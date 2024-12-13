d7ae551eb2f26b21f3cd813142a6458d649e2184
Josh Stringer <joshstringer@live.com>
2024-12-12 18:31:12 -0600

    now dungeonman can move to any square with only a click of the mouse

--- client/src/components/Room1/Room1.jsx
+++ client/src/components/Room1/Room1.jsx
@@ -3,6 +3,11 @@ import './Room1.css';
 
 function Room1() {
     const squares = new Array(120).fill(null);
+    const [dungeonManIndex, setDungeonManIndex] = useState(60);
+
+    const handleSquareClick = (index) => {
+        setDungeonManIndex(index);
+    }
 
     return (
         <>
@@ -12,8 +17,11 @@ function Room1() {
 
             <div className='room1Box'>
                 {squares.map((_, index) => (
-                    <div key={index} className={`square ${index === 60 ? 'center' : ''}`}>
-                        {index === 60 && <img src='/DungeonMan.png' alt='DungeonMan' />}
+                    <div key={index} 
+                         className={`square ${index === 60 ? 'center' : ''}`}
+                         onClick={() => handleSquareClick(index)}
+                    >
+                        {index === dungeonManIndex && <img src='/DungeonMan.png' alt='DungeonMan' />}
                     </div>
                 ))}
             </div>
