4d6cbcba8d83475a939c28f89898b11041d6598e
Josh Stringer <joshstringer@live.com>
2024-12-12 19:20:06 -0600

    further edited character modal

--- client/src/components/Actions/Character/Character.jsx
+++ client/src/components/Actions/Character/Character.jsx
@@ -5,9 +5,11 @@ function Character({ onClose }) {
     return (
         <div className='character-overlay' onClick={onClose}>
             <div className='character-info' onClick={(e) => e.stopPropagation()}>
-                <h2>Character Page</h2>
-                <p>Stuff about Dungeon Man</p>
-                <button onClick={onClose}>Close</button>
+                <button className='tiny5-regular menuButton'>CHARACTER</button><br></br>
+                <button className='tiny5-regular menuButton'>INVENTORY</button><br></br>
+                <button className='tiny5-regular menuButton'>SAVE/LOAD</button><br></br>
+                <button className='tiny5-regular menuButton'>OPTIONS</button><br></br>
+                <button className='tiny5-regular menuButton' onClick={onClose}>CLOSE</button>
             </div>
         </div>
     )
